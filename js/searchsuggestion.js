99643296aacd4b34f9e2b69d894e66e704bc83ae
NityamKumar1 <83266449+NityamKumar1@users.noreply.github.com>
2021-04-27 11:13:11 +0530

    Web

---
+++ js/searchsuggestion.js
@@ -0,0 +1,11 @@
+let suggestions = [
+    "How to Install & Use Material Theme in Android Studio",
+    "How to Install & Setup VS Code for running C++ Programs",
+    "How to install Virtual box in windows 10 2021",
+    "How to type hindi in Window 10",
+    "How to Install Python 3.9 in Windows in 2021",
+    "10 Keyboard Shortcuts You Might Not Know",
+    "How to Find Your Computer IP Address on Windows 10",  
+    "Nityam's privacy policy and terms of services",
+    "DialogFlow – Introduction and Basics",
+];
